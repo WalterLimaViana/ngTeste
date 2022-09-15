1c14879d5a4b25004bbbb6409ae0737fdd1dfceb
WalterLimaViana <walter.lima.viana@gmail.com>
2022-09-14 23:52:52 -0300

    usando o testBed para criar um módulo

--- src/app/shared/components/like-widgets/like-widget.component.spec.ts
+++ src/app/shared/components/like-widgets/like-widget.component.spec.ts
@@ -1,10 +1,13 @@
+import { TestBed } from '@angular/core/testing';
 import { UniqueIdService } from './../../services/unique-id/unique-id.service';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
   let component: LikeWidgetComponent = null;
-  beforeEach(() => {
-    component = new LikeWidgetComponent(new UniqueIdService());
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LikeWidgetComponent],
+    }).compileComponents();
   });
 
   it('', () => {});
